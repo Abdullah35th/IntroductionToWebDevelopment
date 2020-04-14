3983a9a65beb0130e145e26989f548aca251bb05
Abdullah Nazari <nazariabdullah56@gmail.com>
2020-04-14 16:14:49 -0400

    netbeans ide

---
+++ classwork/HelloWorld/src/main/java/com/abdullah/helloworld/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abdullah Nazari
+ */
+
+package com.abdullah.helloworld; //my package name
+
+public class Hello {
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");
+            System.out.println("My name is Harry!");
+
+    } 
+   
+}
